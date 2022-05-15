512a4a8c082375eb36171105315bac39337ab9cd
J4V1R6 <javirosra@gmail.com>
2022-05-15 18:11:34 +0200

    Interfaz inicial

--- client/Client.java
+++ client/Client.java
@@ -11,129 +11,139 @@ public class Client extends JFrame implements ActionListener{
     private JMenuBar menu_bar;
     private JMenuItem menuItem_calendario, menuItem_cuenta, menuItem_fin;
     private JPanel panel_inicio,panel_calendario,panel_cuenta;
+    private JButton boton_login;
 
     public Client() {
+        
+        this.setLocationRelativeTo(null); //Esto permite que la ventana aparezca al centro
+        this.setLayout(null); //Layout absoluto
 
         //Se crea el panel de inicio
-        this.setLayout(null); //Layout absoluto
-        panel_inicio = panelInicio();
+        //panel_inicio = panelInicio();
+        //this.add(panel_inicio);
+        
+        JLabel label_correo = new JLabel("Correo");
+        label_correo.setBounds(40,30,200,30);
+        this.add(label_correo);
 
-        //Se crea el panel de cuenta
-        //panel_cuenta = panelCuenta();
+        JTextField field_correo = new JTextField("correo",20);
+        field_correo.setBounds(40,60,200,30);
+        this.add(field_correo);
 
-        //Se crea el panel de calendario
-        //panel_calendario = panelCalendario();
+        JLabel label_pass = new JLabel("Contraseña");
+        label_pass.setBounds(40,110,200,30);    
+        this.add(label_pass);
 
-        //Creamos la barra de menu de la ventana
-        menu_bar=menuBar();
+        JTextField field_pass = new JTextField("contraseña",20);
+        field_pass.setBounds(40,140,200,30);   
+        this.add(field_pass);
 
-        //Asociamos los elementos al JFrame
-        this.setJMenuBar(menu_bar);
-        this.add(panel_inicio,BorderLayout.CENTER);
+        boton_login = new JButton("Inicar Sesión");
+        boton_login.setBounds(40,190,200,25);
+        boton_login.addActionListener(this);     
+        this.add(boton_login);
 
-        //Configurar y mostrar JFrame
-        initPantalla();
-    }
+        JButton boton_register = new JButton("Crear Cuenta");
+        boton_register.setBounds(40,230,200,25);        
+        this.add(boton_register);
 
-    private void initPantalla() {
+        this.setBackground(Color.decode("#ACBFC5"));
 
-        this.setTitle("TotalAgenda"); //Título del JFrame
-        this.setSize(800,550); //Dimensiones del JFrame (ancho,alto)
-        this.setMinimumSize(new Dimension(800,550)); //(ancho,alto)
-        this.setLocationRelativeTo(null); //Esto permite que la ventana aparezca al centro
-        this.setResizable(false); //Redimensionable
+        // Configuramos la ventana
+        this.setTitle("TotalAgenda - Inicio Sesión"); //Título del JFrame
+        this.setSize(280,300); //Dimensiones del JFrame (ancho,alto)
+        this.setResizable(false); //No redimensionable
+        //this.setMinimumSize(new Dimension(800,550)); //(ancho,alto)
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar proceso al cerrar ventana
         this.setVisible(true); //Mostrar JFrame
+
+        //Instancia un objeto BorderLayout con una separacion de 15px en horizonal y vertical
+        //BorderLayout miBorderLayout = new BorderLayout(15,15);
+
+        //Uso este BorderLayout para que sea el controlador de posicionamiento de mi objeto JFrame
+        //this.setLayout(miBorderLayout);
     }
 
     /* Método que implementa las acciones de cada ítem de menú */
     public void actionPerformed(ActionEvent e) {
         if (e.getSource()==menuItem_calendario) {
-            setSize(640,480);
+            this.getContentPane().removeAll();
+            this.getContentPane().invalidate();
+            this.getContentPane().add(panelCalendario());
+            this.getContentPane().revalidate();
         }
         if (e.getSource()==menuItem_cuenta) {
-            setSize(1024,768);
+            this.getContentPane().removeAll();
+            this.getContentPane().invalidate();
+            this.getContentPane().add(panelCuenta());
+            this.getContentPane().revalidate();
+        }
+        if (e.getSource()==boton_login) {
+           System.out.println("Se ha pulsado el botón boton_login");
+           cambioMarco();
         }
         if (e.getSource()==menuItem_fin) {
             System.out.println("Se llama a finalizar la aplicación");
             System.exit(0);
         }
     }
-    private JPanel panelInicio(){
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(6,1,15,15)); // filas,columnas,pixeles entre filas,pixeles entre columnas
 
-        JLabel label_correo = new JLabel("Correo");
-        //label_correo.setMinimumSize(new Dimension(20, 5));
-        //label_correo.setPreferredSize(new Dimension(20, 5));
-        //label_correo.setMaximumSize(new Dimension(20, 5));
-        //label_correo.setLocation(400,100);
-        JTextField field_correo = new JTextField("correo",20);
-        //label_correo.setMinimumSize(new Dimension(20, 5));
-        //label_correo.setPreferredSize(new Dimension(20, 5));
-        //label_correo.setMaximumSize(new Dimension(20, 5));
-        //label_correo.setLocation(400,130);
-        JLabel label_pass = new JLabel("Contraseña");
-        //label_correo.setMinimumSize(new Dimension(20, 5));
-        //label_correo.setPreferredSize(new Dimension(20, 5));
-        //label_correo.setMaximumSize(new Dimension(20, 5));
-        //label_correo.setLocation(400,160);        
-        JTextField field_pass = new JTextField("contraseña",20);
-        //label_correo.setMinimumSize(new Dimension(20, 5));
-        //label_correo.setPreferredSize(new Dimension(20, 5));
-        //label_correo.setMaximumSize(new Dimension(20, 5));
-        //label_correo.setLocation(400,190);        
-        JButton boton_login = new JButton("Inicar Sesión");
-        //label_correo.setMinimumSize(new Dimension(20, 5));
-        //label_correo.setPreferredSize(new Dimension(20, 5));
-        //label_correo.setMaximumSize(new Dimension(20, 5));
-        //label_correo.setLocation(400,220);        
-        JButton boton_register = new JButton("Crear Cuenta");
-        //label_correo.setMinimumSize(new Dimension(20, 5));
-        //label_correo.setPreferredSize(new Dimension(20, 5));
-        //label_correo.setMaximumSize(new Dimension(20, 5));
-        //label_correo.setLocation(400,250);        
+    private void cambioMarco(){
+        this.getContentPane().removeAll();
+        this.getContentPane().invalidate();
 
-        panel.add(label_correo);
-        panel.add(field_correo);
-        panel.add(label_pass);
-        panel.add(field_pass);
-        panel.add(boton_login);
-        panel.add(boton_register);
+        this.setTitle("TotalAgenda"); //Título del JFrame
+        this.setSize(600,500); //Dimensiones del JFrame (ancho,alto)
+        this.setResizable(true); //Redimensionable
 
-        return panel;
+        //Creamos la barra de menu de la ventana
+        menu_bar=menuBar();
+        this.setJMenuBar(menu_bar);
+
+        JLabel label_correo = new JLabel("PRUEBA");
+        label_correo.setBounds(40,30,200,30);
+        this.add(label_correo);
+
+
+        this.getContentPane().add(panelCalendario());
+        this.getContentPane().revalidate(); 
+
     }
     private JPanel panelCuenta(){
         JPanel panel = new JPanel();
+        panel.setBackground(Color.GREEN);
+
         JLabel label_correo = new JLabel("Correo");
-        JTextField field_correo = new JTextField("correo",20);
-        JLabel label_pass = new JLabel("Contraseña");
-        JTextField field_pass = new JTextField("contraseña",20);
-        JButton boton_login = new JButton("Inicar Sesión");
-        JButton boton_register = new JButton("Crear Cuenta");
+        //JTextField field_correo = new JTextField("correo",20);
+        //JLabel label_pass = new JLabel("Contraseña");
+        //JTextField field_pass = new JTextField("contraseña",20);
+        //JButton boton_login = new JButton("Inicar Sesión");
+        //JButton boton_register = new JButton("Crear Cuenta");
         panel.add(label_correo);
-        panel.add(field_correo);
-        panel.add(label_pass);
-        panel.add(field_pass);
-        panel.add(boton_login);
-        panel.add(boton_register);
+        //panel.add(field_correo);
+        //panel.add(label_pass);
+        //panel.add(field_pass);
+        //panel.add(boton_login);
+        //panel.add(boton_register);
 
         return panel;
     }
     private JPanel panelCalendario(){
         JPanel panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+
         JLabel label_correo = new JLabel("Correo");
-        JTextField field_correo = new JTextField("correo",20);
-        JLabel label_pass = new JLabel("Contraseña");
-        JTextField field_pass = new JTextField("contraseña",20);
-        JButton boton_login = new JButton("Inicar Sesión");
-        JButton boton_register = new JButton("Crear Cuenta");
+        //JTextField field_correo = new JTextField("correo",20);
+        //JLabel label_pass = new JLabel("Contraseña");
+        //JTextField field_pass = new JTextField("contraseña",20);
+        //JButton boton_login = new JButton("Inicar Sesión");
+        //JButton boton_register = new JButton("Crear Cuenta");
         panel.add(label_correo);
-        panel.add(field_correo);
-        panel.add(label_pass);
-        panel.add(field_pass);
-        panel.add(boton_login);
-        panel.add(boton_register);
+        //panel.add(field_correo);
+        //panel.add(label_pass);
+        //panel.add(field_pass);
+        //panel.add(boton_login);
+        //panel.add(boton_register);
 
         return panel;
     }
