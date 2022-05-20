3d625d682e870d492d316ecfb94198b9455a51a1
JohnStopped <jpc.7.f1@gmail.com>
2022-05-20 20:19:55 +0200

    DDBB files end

--- users.sql
+++ users.sql
@@ -2,4 +2,5 @@ drop table if exists users cascade; -- se añade cascade porque es referenciada
 
 create table users (
 email varchar(40) primary key,
-passwd varchar(30));
+passwd varchar(30),
+session_id integer);
